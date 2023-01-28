d5d340ac2a57d7e1861ec5aab57abca167d7ac3c
Kingdon Barrett <kingdon@weave.works>
2023-01-28 13:06:16 -0500

    parallelize
    Signed-off-by: Kingdon Barrett <kingdon@weave.works>

--- cmd/kubeconfig-ca-fetch/main.go
+++ cmd/kubeconfig-ca-fetch/main.go
@@ -5,16 +5,44 @@ import (
 	"crypto/tls"
 	b64 "encoding/base64"
 	"fmt"
+	"log"
+	"net"
 	"net/http"
+	"time"
 
 	"encoding/pem"
 
 	"go.step.sm/crypto/pemutil"
 )
 
+type Base64Result struct {
+	Name  string
+	Url   string
+	Cert  string
+	time_ int64
+}
+
+func getBase64Result(client *http.Client, name string, url string, ch chan *Base64Result) {
+	result := Base64Result{name, url, "", time.Now().UnixNano()}
+
+	result.Cert, _ = getCertCaBase64(url, client)
+	result.time_ = time.Now().UnixNano() - result.time_
+
+	ch <- &result
+}
+
+var timeout = time.Duration(2 * time.Second)
+
+func dialTimeout(network, addr string) (net.Conn, error) {
+	return net.DialTimeout(network, addr, timeout)
+}
+
 func main() {
 	client := &http.Client{
+		// NB: This is not the timeout we needed!
+		// Timeout: 5 * time.Second,
 		Transport: &http.Transport{
+			Dial: dialTimeout,
 			TLSClientConfig: &tls.Config{
 				InsecureSkipVerify: true,
 			},
@@ -32,20 +60,66 @@ func main() {
 		"somtochi":       "somtochi.turkey.local",
 		"another-test":   "another-test.turkey.local",
 		"limnocentral":   "limnocentral.turkey.local",
+		"management":     "loft.loft.svc.cluster.local",
 	}
-	mout := map[string]string{}
+	// result holds a cert from certs[0], or an empty string for cert
+	ch := make(chan *Base64Result)
 
+	// Call http routine as an asynchronous function
 	for k, v := range m {
 		url := fmt.Sprintf("https://%s", v)
-		ca, err := getCertCaBase64(url, client)
-		if err != nil {
-			// log.Println(err)
-			// return
-		}
-		mout[k] = ca
+		// getBase64Result always returns a result regardless of failure
+		go getBase64Result(client, k, url, ch)
 	}
 
-	// Let's print our Kubeconfig
+	// m is the "input" map and it has the same length as the finished output map
+	// but failed connections will be empty certs, get omitted from the kubeconfig
+	out := map[string]string{}
+	fillOutputMap(m, out, ch)
+	printKubeconfig(m, out)
+}
+
+func getCertCaBase64(url string, client *http.Client) (ret string, err error) {
+	resp, err := client.Get(url)
+	if err != nil {
+		return "", err
+	}
+
+	certs := resp.TLS.PeerCertificates
+	p, err := pemutil.Serialize(certs[0])
+	if err != nil {
+		return "", err
+	}
+	var buf bytes.Buffer
+	err = pem.Encode(&buf, p)
+	if err != nil {
+		return "", err
+	}
+
+	str := b64.StdEncoding.EncodeToString(buf.Bytes())
+	return str, nil
+}
+
+func fillOutputMap(m map[string]string, out map[string]string, ch chan *Base64Result) {
+	// set doLog := true to enable logging to stderr
+	doLog := false
+	for i := 0; i < len(m); i++ {
+		c := <-ch
+		name := c.Name
+		cert := c.Cert
+		out[name] = cert
+		// Only print to stderr if logging is enabled
+		if doLog {
+			if c.Cert == "" {
+				log.Printf("Failed to reach %s (%s) after %d ms\n", c.Name, c.Url, c.time_/1e6)
+			} else {
+				log.Printf("Reached %s in %dms\n", c.Url, c.time_/1e6)
+			}
+		}
+	}
+}
+
+func printKubeconfig(min map[string]string, mout map[string]string) {
 	fmt.Println("apiVersion: v1\nclusters:")
 
 	// clusters
@@ -53,7 +127,7 @@ func main() {
 		if cert != "" {
 			fmt.Println("  - cluster:")
 			fmt.Printf("        certificate-authority-data: %s\n", cert)
-			fmt.Printf("        server: https://%s\n", m[name])
+			fmt.Printf("        server: https://%s\n", min[name])
 			fmt.Printf("    name: %s\n", name)
 		}
 	}
@@ -81,25 +155,4 @@ users:
                 extra-scopes: groups
                 idp-issuer-url: https://dex.howard.moomboo.space
             name: oidc`)
-}
-
-func getCertCaBase64(url string, client *http.Client) (ret string, err error) {
-	resp, err := client.Get(url)
-	if err != nil {
-		return "", err
-	}
-
-	certs := resp.TLS.PeerCertificates
-	p, err := pemutil.Serialize(certs[0])
-	if err != nil {
-		return "", err
-	}
-	var buf bytes.Buffer
-	err = pem.Encode(&buf, p)
-	if err != nil {
-		return "", err
-	}
-
-	str := b64.StdEncoding.EncodeToString(buf.Bytes())
-	return str, nil
 }
